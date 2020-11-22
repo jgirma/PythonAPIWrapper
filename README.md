ab218e7f19782f33db93502cb6812e83a71d70ab
Jonathan Girma <jgirma@ualberta.ca>
2020-11-21 18:01:24 -0700

    updates

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # PythonAPIWrapper
 
-This code will create an API Wrapper to pull from tmdb.com following the tutorial that was provided https://semaphoreci.com/community/tutorials/building-and-testing-an-api-wrapper-in-python
+This code will create an API Wrapper to pull from tmdb.com following the tutorial that was provided by https://semaphoreci.com/community/tutorials/building-and-testing-an-api-wrapper-in-python repo:https://github.com/kevgathuku/tmdbwrapper
 
 For future projects using API wrapping look at the following repos for some guidance:
 https://github.com/xiongchiamiov/pyfixit
--- tests/test_tmdbwrapper.py
+++ tests/test_tmdbwrapper.py
@@ -24,15 +24,15 @@ def test_tv_popular():
     assert isinstance(response['results'][0], dict)
     assert set(tv_keys).issubset(response['results'][0].keys())
 
-# # The following code is going to take test the tv show's info to
-# # ensure that it contains all the values in the tv_keys
-# @vcr.use_cassette('tests/vcr_cassettes/tv-info.yml')
-# def test_tv_info(tv_keys):
-#     """Tests an API call to get a TV show's info"""
-#
-#     tv_instance = TV(1396)
-#     response = tv_instance.info()
-#
-#     assert isinstance(response, dict)
-#     assert response['id'] == 1396, "The ID should be in the response"
-#     assert set(tv_keys).issubset(response.keys()), "All Keys should be in the response"
+# The following code is going to take test the tv show's info to
+# ensure that it contains all the values in the tv_keys
+@vcr.use_cassette('tests/vcr_cassettes/tv-info.yml')
+def test_tv_info(tv_keys):
+    """Tests an API call to get a TV show's info"""
+
+    tv_instance = TV(1396)
+    response = tv_instance.info()
+
+    assert isinstance(response, dict)
+    assert response['id'] == 1396, "The ID should be in the response"
+    assert set(tv_keys).issubset(response.keys()), "All Keys should be in the response"
